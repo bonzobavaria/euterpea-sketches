642c4228008edabe1dbb767f43db9052e18abbdf
Kevin Waddle <bonzobavaria@gmail.com>
2019-09-28 12:17:45 -0500

    Copy foldy to Utils as unravel

--- Scales.hs
+++ Scales.hs
@@ -6,16 +6,17 @@ type Scale = [Int]
 
 minor = aeolian :: Scale
 major = ionian
-ionian = [0,2,4,5,7,9,11]
-dorian = [0,2,3,5,7,9,10]
-phrygian = [0,1,3,5,7,8,10]
-lydian = [0,2,4,6,7,9,11]
-mixolydian = [0,2,4,5,7,9,10]
-aeolian = [0,2,3,5,7,8,10]
-locrian = [0,1,3,5,6,8,10]
-harmonicMinor = [0,2,3,5,7,8,11]
-melodicMinor = [0,2,3,5,7,9,11]
-hungarianMinor = [0,2,3,6,7,8,10]
+chromatic =       [0..11]
+ionian =          [0,2,4,5,7,9,11]
+dorian =          [0,2,3,5,7,9,10]
+phrygian =        [0,1,3,5,7,8,10]
+lydian =          [0,2,4,6,7,9,11]
+mixolydian =      [0,2,4,5,7,9,10]
+aeolian =         [0,2,3,5,7,8,10]
+locrian =         [0,1,3,5,6,8,10]
+harmonicMinor =   [0,2,3,5,7,8,11]
+melodicMinor =    [0,2,3,5,7,9,11]
+hungarianMinor =  [0,2,3,6,7,8,10]
 
 -- Ex: scale D major = map (+2) major
 scale :: PitchClass -> Scale -> Scale
--- Utils.hs
+++ Utils.hs
@@ -1,5 +1,6 @@
 module Utils (
-  addDur
+  addDur,
+  unravel
 ) where
 
 import Euterpea
@@ -7,3 +8,19 @@ import Euterpea
 addDur :: Dur -> [(Dur -> Music a)] -> Music a
 addDur d ns = line [n d | n <- ns]
 
+unravel :: [[AbsPitch]] -> [AbsPitch]
+unravel [] = []
+unravel (x:xs) =
+  let func = ((<*>) . ((<$>) (+)))
+  in foldr func x xs
+
+-- Examples:
+--   unravel [[0,3,7,12],[0..3],[0,12]]
+-- Ascending minor arpeggios, going up by half-step, then
+-- the same sequence up an octave. So the first value is the innermost
+-- arpeggiated shape.
+-- 
+-- unravel $ reverse [[0..3],[0,5..15],[0,12]
+-- = Octaves, moving up in quintal motion, with the pattern repeating 4 times
+-- and moving up a half-step each time, so the first value is the outermost
+-- shape, i.e. the one that completes only once and has the longest duration.
